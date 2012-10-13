6d657b35ab90a52636ffe43573edb2cf36f5e8b4
David Crutchfield <crutchfield@interactivetouchbooks.com>
2012-10-13 15:35:17 -0700

    Added more tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,1 @@
-def full_page_title(partial_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  return "#{base_title} | #{partial_title}" unless partial_title.blank?
-  return base_title
-end
+include ApplicationHelper
