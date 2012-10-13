6209d9550ce7fd13d92a14aa57c885f778d8a1e6
David Crutchfield <crutchfield@interactivetouchbooks.com>
2012-10-13 14:05:33 -0700

    Finish layout and routes

---
+++ spec/support/utilities.rb
@@ -0,0 +1,5 @@
+def full_page_title(partial_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  return "#{base_title} | #{partial_title}" unless partial_title.blank?
+  return base_title
+end
