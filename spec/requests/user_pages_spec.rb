1f0b1cc8bbc918adf5ab373ec69082934153f9ca
David Crutchfield <crutchfield@interactivetouchbooks.com>
2012-10-17 18:54:45 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -35,6 +35,7 @@ describe "User pages" do
         let(:user) { User.find_by_email "user@example.com" }
         it { should have_selector "title",                   text: user.name }
         it { should have_selector "div.alert.alert-success", text: "Welcome" }
+        it { should have_link "Sign out",                    href: signout_path }
       end
     end
   end
