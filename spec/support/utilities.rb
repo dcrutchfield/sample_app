b024ec9dbe724f472fb27e7ecdfa25ea0764ab47
David Crutchfield <crutchfield@interactivetouchbooks.com>
2012-10-19 23:59:37 -0700

    Finish user REST implementation

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,11 @@
 include ApplicationHelper
 
-def signin(user = nil)
-  fill_in "Email",    with: user.email unless user.nil? 
-  fill_in "Password", with: user.password unless user.nil?
+def sign_in(user = nil)
+  visit signin_path
+  unless user.nil?
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+  end
   click_button "Sign In"
 end
 
